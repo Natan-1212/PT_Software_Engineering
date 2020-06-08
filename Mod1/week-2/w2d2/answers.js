69e7833d73a2648114923067a302883b2bdf724e
PSCBUS_105 <pscbus_105@PSCBUS-105.local>
2020-06-08 11:01:01 -0400

    Get Even answered

--- Mod1/week-2/w2d2/answers.js
+++ Mod1/week-2/w2d2/answers.js
@@ -9,7 +9,12 @@
 
 ////////Easy Goinng ////////
 
-for(let i = 1; i <= 20; i++){
+/*for(let i = 1; i <= 20; i++){
     console.log(i);
-}
+} */
 
+for (let i = 0; i <= 200; i++){
+    if(i % 2 === 0){
+        console.log(i);
+    }
+}
