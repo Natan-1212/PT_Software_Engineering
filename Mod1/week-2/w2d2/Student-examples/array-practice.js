237dfd48fe3b9ce251e9ee180d090e264eb055e5
PSCBUS_105 <pscbus_105@PSCBUS-105.local>
2020-06-08 10:54:14 -0400

    Easy Going answered

---
+++ Mod1/week-2/w2d2/Student-examples/array-practice.js
@@ -0,0 +1,65 @@
+/*const sayHello = "hello I am Natan";
+const num =12;
+const bool = true;
+console.log(bool);
+console.log(sayhello);*/
+//const ghostBusters = ["venkman", "Spengler", "Stanz", "Zeddemore"
+//, "Meltinez" , "barrett" ,"Tully", "Jose"];
+/*onsole.log(ghostBusters[1]);
+console.log(ghostBusters[3]);
+console.log(ghostBusters.length);// number oe elements
+cost .log(ghostBusters);// here we have jose last 
+ghostBusters[0] = "kimmy";
+console.log(ghostBusters);*/
+//const numbers = [21, 18, 5, 3, 2, 1, 1];
+/*numbers[2] = null;
+console.log(numbers);
+number[0] = number[0] * 20;
+console.log(numbers);*/
+//const foodSelection = ["pizza", "apple", "seew", "arti", "tea"];
+/*const kitchenSink = ["Dirty spoon", "sponge", "plate with gunk", "soap", "water"]; // 5 items so last index = 4
+/*for(let iteratorNumber = 0; iteratorNumber < kitchenSink.length; iteratorNumber++) {
+    console.log(iteratorNumber);
+    console.log("This is my kitchen sink " + kitchenSink[iteratorNumber]);*/
+/*for (let i = 0; i< kitchenSink.length; i++){
+    if (i % 2 === 0)
+    console.log(kitchenSink[i]);
+
+
+}*/
+/*for (let i = 0; i< d.length; i++){
+    const element = kitchenSink[i];
+    console.log(element);*/
+   /* const drSeuss = ["Cat in the Hat", "Green Eggs and Ham", "Grinch", "Thing One", "Thing Two",
+     "Cindy Loo Who", "JoJo McDodd"];
+     const drSeuss = ["Cat in the Hat", "Green Eggs and Ham", "Grinch", "Thing One", "Thing Two", "Cindy Loo Who", "JoJo McDodd"];
+// iterate through every element in array and console log it
+for (let i = 0; i < drSeuss.length; i++) {
+    const element = drSeuss[i];
+    console.log(element);
+}
+// only console log
+for (let i = 0; i < drSeuss.length; i++) {
+    // if(i % 2 === 0) this is the if statement/conditional to check for an even number
+    if(i % 2 !== 0) {
+        console.log(drSeuss[i]);
+    
+    }
+}
+/*
+for (let i = 0; i < drSeuss.length; i++) {
+    if(i % 2 !== 0) {
+        const element = drSeuss[i];
+        console.log(element);
+    }
+}
+*/
+const favMovies = ["Fridays", "Love and Basketball", "Borat", "Game of Thrones", 
+"Friends", "Breaking Bad", "Two and a half men ", "Olympus" , "runaway jurry", "the last OG"];
+//console.log(favMovies.indexOf("Friends"));
+//const count = favMovies.push("Philadelphia");
+//console.log(count);
+const firstElement = favMovies
+
+
+
---
+++ Mod1/week-2/w2d2/answers.js
@@ -0,0 +1,15 @@
+ ////////////////////PART ONE///////////////
+ //////////// Ranking My Comfort with HTML and CSS video contents//////
+ /////HTML Intro /////// 5 - I got this!///////
+ //////HTML Elements/////4 -feel pretty good ,need some extra review///
+ //////HTML CSS Intro/////5-I got this!/////////
+ /////CSS Basics /////////4--feel pretty good ,need some extra review///
+
+///////////////////PART TWO-JavaScript Reps //////////////////////////
+
+////////Easy Goinng ////////
+
+for(let i = 1; i <= 20; i++){
+    console.log(i);
+}
+
