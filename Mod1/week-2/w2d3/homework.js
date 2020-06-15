10790de498f643f02fb9a416cf4c41d02965d875
PSCBUS_105 <pscbus_105@PSCBUS-105.local>
2020-06-15 15:59:12 -0400

    Homework on  Beginner's Guide to UX/UI

---
+++ Mod1/week-2/w2d3/homework.js
@@ -0,0 +1,70 @@
+///Discuss - Why do you think Heinz made this change?
+
+/*user experience (UX) refers to any interaction a user has with a product or service.
+ UX design considers each and every element that shapes this experience, 
+ how it makes the user feel, and how easy it is for the user to accomplish their
+  desired tasks. UI design is the look and feel, the presentation and 
+  the interactivity of a product. When Heinz made changes on the tomato ketchup bottles, 
+  the updated one appears easy to handle, user friendly and easier to put it upside down 
+  so that it will be convenient to add the katchup whereever the customer needs and it 
+  seems in line with the concepts of UX/UI Design in web developmet */
+
+
+  //Discuss - How does this apply to web development?
+
+  /*Watching the Super Mario game helps me to understand that the UX skills learned from 
+  the game are crucial to design patterns, flows, information structures, and 
+  user observation skills to tackle UX/UI challenges arising between developers
+   and other user's successfull experiences. */
+
+///A user interface is like a joke. If you have to explain it, it's not that good.
+/*When someone tells you a joke, they are expecting a laugh from you. And it feels odd if you don’t 
+understand the joke and no response comes from your side.
+The person who is telling the joke has to explain it, and the situation no longer remains humorous.
+Similarly in case of  user interface, when a user comes to your product, they should be able to 
+find the possible actions they can perform with the design. A design should be self-explanatory and 
+understandable for the user by making ease of use its priority. 
+Ease of use is the top most important feature frustrates users when missing. Hence, a good joke 
+and UI needs to be simple , self-explanatory and understandable for the audiences and users
+in order to get the maximum output.*/
+
+////Discuss - How does this apply to web development?/////
+
+/*In the vedio Dan Norman explained that a good and Human-centred design door incorrporates 
+fundamental principles; 
+*Discoverability- the user cannot find it, it does not exist. It must be made clear as a bell 
+what actions are possible in a UI in less than a microsecond — for example, labeling icons as a best practice.
+
+*Feedback- It means that constant visibility of system status, i.e., the system should make sure that: 
+users are kept informed about what’s going on and there is continuous information about the results of 
+their actions and the current state of the product (system). Users has to be updated and 
+there should not be a gap of information (UX/UI should also take this tenet under cosideration )
+
+*Affordances- an object’s affordance is not determined solely by design but by the way a user interacts with it. 
+An object’s affordance is determined by user interaction and it has to be user friendly. */
+
+////What's a site that you use all the time that has good UX? Facebook 
+
+/*How is that site:
+
+*Goal Focused:goal of user interface design is to make the user's interaction as simple and efficient as possible, 
+in terms of accomplishing user goals (user-centered design). Facebook also meets 
+this criteria and my interaction with the cite is simple and efficient whenever i try to access the 
+website 
+
+*Invisible-An invisible UI is when the user experience of a feature is so intuitive that a UI isn’t really required 
+(like Smart Guides in Photoshop, Adobe XD and Sketch — they appear automatically when you move a layer, to help you align it).
+
+*Forgiving-the designs of Facebook also helps people avoid errors and protect them from harm if errors occur.This 
+is done by providing confimations and warnings whenever one tries to share a post 
+or pictures on Facebook
+
+*Consistent-consistency is the thread that ties together elements in a single design. The 
+design Facebook website has since its inception has relatively the same kind of buttons
+and locations of the buttons to share , like , post messages.
+
+Smooth Onboarding- onboarding experience is a way to introduce users to a new product, app, or feature. 
+Onboarding UX is the design of a flow or series of flows that give the user a guided introduction to the product, 
+set up some initial preferences, or point out critical UI elements in an interface. Facebook also 
+fulfills this criteria as a user friendly interface. */
+
