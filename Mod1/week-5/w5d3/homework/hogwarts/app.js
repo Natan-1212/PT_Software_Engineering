1a61f29c2c24b05ca0971fcef0aee0d0c9302b64
PSCBUS_105 <pscbus_105@PSCBUS-105.local>
2020-07-10 13:52:01 -0400

    I added my first element and it was fun

--- Mod1/week-5/w5d3/homework/hogwarts/app.js
+++ Mod1/week-5/w5d3/homework/hogwarts/app.js
@@ -1,5 +1,13 @@
-if (typeof jQuery == 'undefined'){
+/*if (typeof jQuery == 'undefined'){
   console.log('oops! I still have to link my jQuery properly!');
 } else {console.log('I did it! I linked jQuery and this js file!')};
 //console.log("logging correcttly");
-//console.log("Welcome To Homework Hogwarts!");
+//console.log("Welcome To Homework Hogwarts!");*/
+$(() => {
+  // put jQuery in her
+
+});
+const $container = $('.container')
+console.log($container);
+$('body').append('<h1/>').text("Hogwarts");
+
