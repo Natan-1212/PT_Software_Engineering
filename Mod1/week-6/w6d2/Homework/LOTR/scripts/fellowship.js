bcc468d789a04dbf67549890652e7d389fc01c63
PSCBUS_105 <pscbus_105@PSCBUS-105.local>
2020-07-20 19:48:34 -0400

    Chapter 9 complete - Updated Gandalf

--- Mod1/week-6/w6d2/Homework/LOTR/scripts/fellowship.js
+++ Mod1/week-6/w6d2/Homework/LOTR/scripts/fellowship.js
@@ -233,8 +233,16 @@ const forgeTheFellowShip = () => {
 const theBalrog = () => {
 
   // 1. change the 'Gandalf' text to 'Gandalf the White'
+  $("#the-fellowship")
+    .children()
+    .eq(5)
+    .text("Ganfalf the White");
 
   // 2. add a class "the-white" to this element
+  $("#the-fellowship")
+    .children()
+    .eq(5)
+    .addClass("the-white");
 
   // 3. in the style.css file, add a css rule to make elements of the class "the-white" have a white background and a grey border
 
