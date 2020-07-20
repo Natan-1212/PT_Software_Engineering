f3d8d4627acb3480c8d100bf9559e05987e72007
PSCBUS_105 <pscbus_105@PSCBUS-105.local>
2020-07-20 16:46:14 -0400

    I added more elements. My power grows with my dedication and hard work

--- Mod1/week-5/w5d3/homework/hogwarts/app.js
+++ Mod1/week-5/w5d3/homework/hogwarts/app.js
@@ -2,12 +2,34 @@
   console.log('oops! I still have to link my jQuery properly!');
 } else {console.log('I did it! I linked jQuery and this js file!')};
 //console.log("logging correcttly");
-//console.log("Welcome To Homework Hogwarts!");*/
+//console.log("Welcome To Homework Hogwarts!");
 $(() => {
   // put jQuery in her
 
 });
 const $container = $('.container')
-console.log($container);
+//console.log($container);
 $('body').append('<h1/>').text("Hogwarts");
 
+///Year 2
+// Following what you did in Year 1, add the following:*/
+//h2 element with your name
+const $h2 = $('<h2/>')
+console.log($h2)
+$('#container').append('<h2>Natan Ejigu</h2>')
+console.log($h2)
+//const $name = $('<h2/>').text('Natan');
+//$container.append($name);
+//h3 element with your house (Hufflepuff, Gryffindor, Ravenclaw or Slytherin )
+const $h3 = $('<h3/>')
+console.log($h3)
+$('#container').append('<h3>Ravenclaw</h3>')
+// h4 element with your pet's name
+const $h4 = $('<h4/>')
+$('#container').append('<h4>Lala</h4>')
+//this h4 element should have a class with a value of your pet type (owl, cat, toad, etc)
+$('h4').addClass('dog')
+console.log($h4)
+// h4 element with your wand
+const $h4wand = $('<h4/>')
+$('#container').append('<h4>Elder Wand</h4>')
